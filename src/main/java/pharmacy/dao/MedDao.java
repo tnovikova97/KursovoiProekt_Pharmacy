acdcc47e1603a2751ed03b1c26068de82f07d59b
Татьяна <t.novikova.2018@stud.nstu.ru>
2019-12-25 15:54:01 +0600

    Поиск реализован с параметрами

---
+++ src/main/java/pharmacy/dao/MedDao.java
@@ -0,0 +1,27 @@
+package pharmacy.dao;
+/*
+ *  @created 25.12.2019
+ *  @project pharmacy
+ *  @author Татьяна
+ */
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Repository;
+import pharmacy.model.Medicament;
+
+import java.util.List;
+
+@Repository
+public interface MedDao extends JpaRepository<Medicament, Integer> {
+
+//    @Async
+//    public List<Medicament> findByTitleLike(String title);
+
+    @Query(value = "SELECT * FROM medicament WHERE title ilike ?1 and date_end > current_date and quantity > 1", nativeQuery = true)
+    public List<Medicament> findByTitleLike(String title);
+
+
+}
